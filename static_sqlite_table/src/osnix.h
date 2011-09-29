42a567a0688f75bdf60ffaa25dff67f4bef6a93a
pehohlva <pehohlva@gmail.com>
2011-09-29 08:58:33 +0000

    de

---
+++ static_sqlite_table/src/osnix.h
@@ -0,0 +1,31 @@
+#ifndef OSUNIX_H
+#define OSUNIX_H
+
+#include "_tools.h"
+
+#if defined Q_WS_X11
+
+#define COMMAND "bash"
+#define CLEANCONSOLE "clear"
+
+#ifndef WORK_CACHEDIR
+#define WORK_CACHEDIR \
+              QString( "%1/.%2/" ).arg( QDir::homePath() , _PROGRAM_SHORT_NAME )
+#endif
+#define SHORTFILE \
+             QString( "%1_res.apache.xml" ).arg( WORK_CACHEDIR )
+#define INCOMMINGFILE \
+             QString( "%1%2_in.xml" ).arg( WORK_CACHEDIR , _PROGRAM_SHORT_NAME )
+#define CONVERTERFILE \
+             QString( "%1%2_con.xml" ).arg( WORK_CACHEDIR , _PROGRAM_SHORT_NAME )
+#define RESULT_PDF_FILE \
+             QString( "%1%2_export.pdf" ).arg( WORK_CACHEDIR , _PROGRAM_SHORT_NAME )
+
+
+#define LASTDIROPENACTION \
+              QString( "%1_lastdir.dat" ).arg( WORK_CACHEDIR )
+              
+              
+#endif
+#endif
+
