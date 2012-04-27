6bb6933886e5f0bf8d5a1d5141238b6896ef100f
jaeck <jaeck@loaden-development.com>
2012-04-27 00:45:42 +0000

    removed /skin/js/ | wrong directory

--- Mage_1.6.2/skin/js/tiny_mce/plugins/ldmediaplayer/langs/de_dlg.js
+++
@@ -1,8 +0,0 @@
-tinyMCE.addI18n('de.ldmediaplayer_dlg',{
-	title: 'Medien einfügen',
-	legend: 'Mediaplayer - Video & Audio - Einstellungen',
-	mediatype: 'Medientyp auswählen',
-	file: 'Video / Audio Datei',
-	width: 'Breite',
-	height: 'Höhe'
-});
